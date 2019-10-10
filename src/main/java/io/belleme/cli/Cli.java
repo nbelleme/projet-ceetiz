d721ff4b8576a6f5cc31472dc52986c78bad9068
nbelleme <nbelleme@e-biz.fr>
2019-10-10 16:15:25 +0200

    feat : init cli

---
+++ src/main/java/io/belleme/cli/Cli.java
@@ -0,0 +1,38 @@
+package io.belleme.cli;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void main(String... args){
+
+        ;
+
+        try(Scanner command = new Scanner(System.in)) {
+            System.out.println("Enter command: ");
+            boolean running = true;
+
+            while(running){
+
+                switch(command.nextLine()){
+                    case "start":
+                        System.out.println("Machine started!");
+                        break;
+
+                    case "stop":
+                        System.out.println("Machine stopped.");
+                        break;
+
+                    case "exit":
+                        System.out.println("Application Closed");
+                        running = false;
+                        break;
+
+                    default:
+                        System.out.println("Command not recognized!");
+                        break;
+                }
+            }
+        }
+    }
+}
